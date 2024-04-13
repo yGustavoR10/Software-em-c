295ffb0a3ecdd16a54658bec325c3f740ff90392
Gustavo Resende Viana <gustavo.vianar10@gmail.com>
2024-04-13 17:24:12 -0300

    Add files via upload

---
+++ atividade 1 em C/Projetopc12/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vValor;
+    int SomaValor = 0;
+
+
+    printf("Informe o valor1:\n");
+    scanf("%d",&vValor);
+
+    SomaValor += vValor;
+
+    printf("Informe o valor2:\n");
+    scanf("%d",&vValor);
+
+    SomaValor += vValor1,2;
+
+    printf("soma dos dois valores e: %d",SomaValor);
+
+
+    return 0;
+
+
+
+}
